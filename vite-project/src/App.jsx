51a65552e2bcb45aeff4d0aef80af51b53a2e8e6
Bishal Diganta <bishal.diganta@gmail.com>
2023-11-13 15:36:33 +0100

    added footer section

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,21 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Footer } from "/components/footer/Footer";
+
+import { Subscribe } from "/components/Subscribe";
+
 
 function App() {
- 
+    return (
+        <>
+        <Footer />
+        {/* <Router>
+          <Routes>
+            <Route path="/Homes" element={< Footer />} />
+          </Routes>
+        </Router> */}
+        </>
+      );
+
 }
 
 export default App

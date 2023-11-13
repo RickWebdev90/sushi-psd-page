f3e70dd9f69fe373a652432505e3fede71e87f52
NoriesDev <noriesdevelopment@gmail.com>
2023-11-13 12:16:31 +0100

    trying to access assets folder

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,17 @@
 import './App.css'
+import {Section3} from './components/psdNora.jsx'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
- 
+    return (
+        <Router>
+          <Routes>
+            <Route path="/Homes" element={< Section3 />} />
+          </Routes>
+        </Router>
+      );
+    
 }
 
 export default App
